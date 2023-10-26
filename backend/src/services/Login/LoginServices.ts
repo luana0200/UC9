e0a31f350d3d2c5fcc04f769367bc53e25e1ca48
luana0200 <luana7forte@gmail.com>
2023-10-26 15:15:49 -0300

    att

--- backend/src/services/Login/LoginServices.ts
+++ backend/src/services/Login/LoginServices.ts
@@ -18,24 +18,24 @@ class LoginServices {
             throw new Error('Usuário/Senha Incorretos')
         }
 
-        const autenticado = await compare(password, usuario.senha)
+        const autenticado = await compare(password, usuario.senha)//cryptografa a senha 
         if (!autenticado) {
             throw new Error('Usuário/Senha Incorretos')
         }
 
         //criando o token
-        const token = sign({
+        const token = sign({ // a estrutura do token gera uma hash grande
             id: usuario.id,
             email: usuario.email
         },
-            process.env.JWT_SECRET,
+            process.env.JWT_SECRET, //jwt_secret vem da hash do env
             {
                 subject: usuario.id,
                 expiresIn: '1h'
             }
         )
 
-        return {
+        return { //oq retorna pro front-end
             id: usuario.id,
             email: usuario.email,
             token: token
