cbc332246e03cc0eaca062127957d20f1e862bcf
luana0200 <luana7forte@gmail.com>
2023-10-30 17:24:27 -0300

    att

--- frontend/src/Contexts/AuthContext.js
+++ frontend/src/Contexts/AuthContext.js
@@ -9,9 +9,26 @@ export const AuthContext = createContext()
 export default function AuthProvider({ children }) {
 
     const [user, setUser] = useState('')
-    const [token, setToken] = useState('')
+    // const [token, setToken] = useState('')
 
     const isAutenticated = !!user
+    
+    const iToken = localStorage.getItem('@tklogin2023')
+    const token = JSON.parse(iToken)
+
+    async function loginToken() { //verifica se o token é valido
+        try {
+            const resposta = await apiLocal.get('/ListarUsuarioToken', {
+                headers: {
+                    Authorization: 'Bearer ' + `${token}`
+                }
+            })
+            console.log(resposta)
+        } catch (err) {
+
+        }
+    }
+
 
     // se utiliza as chaves para DESCONSTRUIR os arquivos json para usar as informações dentro dele
     async function signIn({ email, password }) {
@@ -28,7 +45,7 @@ export default function AuthProvider({ children }) {
         }
     }
     return (
-        <AuthContext.Provider value={{ signIn }}>
+        <AuthContext.Provider value={{ signIn, loginToken }}>
             {children}
         </AuthContext.Provider>
     )
--- frontend/src/Dashboard/index.js
+++ frontend/src/Dashboard/index.js
@@ -1,10 +1,18 @@
-import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../Contexts/AuthContext'
 
 export default function Dashboard() {
+
+    const { loginToken } = useContext(AuthContext)
+    loginToken()
+    
     return (
         <div>
             <h1>Dashboard</h1>
             <Link to='/Produtos'>Cadastrar Produtos</Link>
+
+
         </div>
     )
 }
