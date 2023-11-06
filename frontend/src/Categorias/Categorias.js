4c9fc1d0936f4fa5851af2c057e67e6c59cab037
luana0200 <luana7forte@gmail.com>
2023-11-06 12:27:37 -0300

    att

---
+++ frontend/src/Categorias/Categorias.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+export default function Categorias() {
+
+    return (
+        <div>
+            <div>
+                <h1>Categorias</h1>
+            </div>
+
+            <div>
+                <form>
+                    <label>Nome:</label>
+                    <input type='text' />
+                </form>
+            </div>
+        </div>
+    );
+}
